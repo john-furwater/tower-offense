a9e07dfb2b670fdc8370b4fdd5f6337b32f4ee20
rainwater <mad.rainwater@gmail.com>
2019-05-06 13:17:28 -0700

    adding controls for moving vertically

---
+++ Assets/Scripts/CannonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 0.2f;
+    Transform myTransform;
+    [SerializeField]
+    float maxDistance = 5.26f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            MoveCannon(Input.GetAxis("Vertical"));
+    }
+
+    private void MoveCannon(float direction)
+    {
+        if(myTransform != null)
+            myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y + (speed * direction));
+    }
+}
