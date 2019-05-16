b4cd7cf8e65b9db792009ee884adc25d21833ef3
greymadr <mad.rainwater@gmail.com>
2019-05-15 21:58:00 -0700

    destroy objects that go off screen

--- Assets/Scripts/Cannon/CrateScript.cs
+++ Assets/Scripts/Cannon/CrateScript.cs
@@ -1,7 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CrateScript : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         transform.SetParent(null, true);
---
+++ Assets/Scripts/Cannon/DestroyOffScreenScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyOffScreenScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.position.y < -20)
+            Destroy(gameObject);
+    }
+}
