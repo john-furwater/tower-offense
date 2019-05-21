5b9a7a6d4d04ecc4796aea33a32b876df7bdd88c
Fyl Johnson <woctaog@gmail.com>
2019-05-20 22:17:41 -0700

    Fix rotation for game pieces (p1 z and x to move, c to rotate)

--- Assets/Scripts/Blocks/CrateScript.cs
+++ Assets/Scripts/Blocks/CrateScript.cs
@@ -4,13 +4,17 @@ public class CrateScript : MonoBehaviour
 {
     AudioSource audioSource;
 
+    public bool canControl;
+
     void Start()
     {
+        canControl = true;
         audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        canControl = false;
         transform.SetParent(GameObject.Find("CrateHolder").transform, true);
         audioSource.Play();
     }
